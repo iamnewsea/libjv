92b30b6aace358a4793b24ddf5c69fea85b14ea9
于新海 <13466396863@163.com>
2017-09-22 12:54:33 +0800

    ..

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 //module.exports = require('./lib/libjv');
 
-import vue_init from "./lib/vue-init"
+import jv_init from "./lib/vue-init"
 import vue_city from "./lib/vue-city"
 import file_upload from "./lib/file-upload"
 
-export default Object.assign({}, vue_init, vue_city, file_upload);
+export default Object.assign(jv_init, vue_city, file_upload);

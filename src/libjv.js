a2ba9ad14de2c8b9114f7678458641d60682b515
于新海 <iamnewsea@163.com>
2017-09-25 23:18:04 +0800

    es5

---
+++ src/libjv.js
@@ -0,0 +1,229 @@
+
+import "./defineProperty"
+
+//---------------------------------------------
+var jv = {};
+
+jv.enumer = function (typeName, json, sep) {
+  var ret = {type: typeName};
+  if (sep) {
+    //枚举三部分： name,value,remark
+    Object.keys(json).forEach(it => {
+      var value = json[it];
+      if (!value.value && (value.value !== 0)) {
+        var sepIndex = value.indexOf(sep);
+        json[it] = {value: parseInt(value.substr(0, sepIndex)), remark: value.substr(sepIndex + 1)};
+      }
+    })
+  }
+  else {
+    var i = -1;
+    Object.keys(json).forEach(it => {
+      var value = json[it];
+      i++;
+      if (!value.value && (value.value !== 0)) {
+        json[it] = {name: it, value: i, remark: json[it]};
+      }
+    })
+  }
+
+  ret.getData = function (key) {
+    if (key) {
+      return json[key];
+    }
+    return Object.keys(json).map(it => {
+      return json[it];
+    });
+  }
+
+  jv[typeName] = ret;
+  return ret;
+}
+
+jv.isPlainObject = function (obj) {
+  // Detect obvious negatives
+  // Use toString instead of jQuery.type to catch host objects
+  if (!obj || ({}).toString.call(obj) !== "[object Object]") {
+    return false;
+  }
+  return true;
+}
+
+jv.random = function (min, max) {
+  if (!min && !max) {
+    return "r" + Math.random().toString(36).slice(3);
+  }
+
+  if (!max) {
+    max = min;
+    min = 0;
+  }
+
+  return min + parseInt(Math.random() * (max - min));
+};
+/**
+ *
+ * @param delayTime  循环暂停时间
+ * @param action     每次循环的回调。
+ * @param times      最多循环次数， 默认为 100
+ * @returns {*}
+ */
+jv.forDelay = function (delayTime, action, times) {
+  times = times || 100;
+
+  if (times == 1) {
+    return;
+  }
+
+  if (action() !== false) {
+    return setTimeout(() => {
+      jv.forDelay(delayTime, action, times - 1);
+    }, delayTime);
+  }
+}
+
+
+/**Java方式的序列化。
+ Java定义：
+ data class role(var name: String = "", var remark: String = "")
+ data class user(var name: String = "", var roles: Array<role> = arrayOf())
+ data class corp(var name: String = "", var users: Array<user> = arrayOf());
+
+ @PostMapping(value = "hi")
+ fun hiPost(qm: corp, session: HttpSession): String {}
+
+ Java格式的数据：(如果数组是简单值的数组，可以使用 "," 连接，如： roles:"销售,开发" )
+ map["name"] = "金维度";
+ map["users[0].name"] = "张三";
+ map["users[0].roles[0].name"] = "销售";
+ map["users[0].roles[0].remark"] = "销售角色";
+
+ map["users[1].name"] = "本四";
+ map["users[1].roles[0].name"] = "开发";
+ map["users[1].roles[0].remark"] = "开发角色";
+ map["users[1].roles[1].name"] = "UI";
+ map["users[1].roles[1].remark"] = "设计师";
+
+ PostBody：
+ name=%E9%87%91%E7%BB%B4%E5%BA%A6&users%5B0%5D.name=%E5%BC%A0%E4%B8%89&users%5B0%5D.roles%5B0%5D.name=%E9%94%80%E5%94%AE&users%5B0%5D.roles%5B0%5D.remark=%E9%94%80%E5%94%AE%E8%A7%92%E8%89%B2&users%5B1%5D.name=%E6%9C%AC%E5%9B%9B&users%5B1%5D.roles%5B0%5D.name=%E5%BC%80%E5%8F%91&users%5B1%5D.roles%5B0%5D.remark=%E5%BC%80%E5%8F%91%E8%A7%92%E8%89%B2&users%5B1%5D.roles%5B1%5D.name=UI&users%5B1%5D.roles%5B1%5D.remark=%E8%AE%BE%E8%AE%A1%E5%B8%88
+
+ Js提交对象：
+ var data =
+ {name:"金维度",users:
+   [
+     {name:"张三",roles:[{name:"销售",remark:"销售角色"}]},
+     {name:"本四",roles:[{name:"开发",remark:"开发角色"},{name:"UI",remark:"设计师"}]}
+   ]
+ }
+ * @param obj
+ */
+
+jv.param_jmap = function (obj) {
+  // var objType = typeof(obj);
+  // if (objType == "string") {
+  //   return obj;
+  // }
+
+  var ret = {};
+  Object.keys(obj).forEach(key => {
+    if (!key) return;
+    if (key.endsWith("_res")) {
+      return;
+    }
+
+    if (key[0] == "_") return;
+
+    var value = obj[key];
+    if (value === undefined) return;
+    if (value === null) return;
+
+    var isMap = function (mapObject) {
+      var isMapValue = mapObject.toString() == "Map";
+      if (!isMapValue) {
+        if (Object.keys(mapObject).findIndex(it => {
+              var code = it.charCodeAt();
+              if (code >= 65 && code <= 90) return true;
+              if (code >= 97 && code <= 122) return true;
+              return false;
+            }) < 0) {
+          isMapValue = true;
+        }
+      }
+      return isMapValue;
+    };
+
+    if (Array.isArray(value)) {
+
+      value = value.filter(it => it != undefined && it != null);
+
+      if (value.length == 0) {
+        ret[key] = "";
+        return;
+      }
+
+      var firstType = typeof(value[0]);
+      if (firstType == "string" || firstType == "number") {
+        ret[key] = value.join(",");
+      }
+
+      for (var i in value) {
+        var item = value[i];
+        if (jv.isPlainObject(item)) {
+          var m = jv.param_jmap(item);
+          var keys = Object.keys(m);
+
+          if (isMap(item)) {
+            keys.forEach(sk => {
+              ret[key + "[" + i + "]['" + sk + "']"] = m[sk];
+            })
+          } else {
+            keys.forEach(sk => {
+              ret[key + "[" + i + "]." + sk] = m[sk];
+            })
+          }
+        }
+      }
+    }
+    else if (jv.isPlainObject(value)) {
+      var m = jv.param_jmap(value);
+      var keys = Object.keys(m);
+
+      if (isMap(value)) {
+        keys.forEach(sk => {
+          ret[key + "['" + sk + "']"] = m[sk];
+        })
+      }
+      else {
+        keys.forEach(sk => {
+          ret[key + "." + sk] = m[sk];
+        })
+      }
+    }
+    else if (value instanceof Date) {
+      ret[key] = value.toDateString();
+    }
+    else {
+      ret[key] = value;
+    }
+  })
+
+  return ret;
+}
+jv.param = function (obj) {
+  var ret = jv.param_jmap(obj);
+  return Object.keys(ret).map(it => {
+    return encodeURIComponent(it) + "=" + encodeURIComponent(ret[it])
+  }).join("&");
+}
+
+//用法： jv.pointObjectValue({a:{b:{c:1}}} , "a.b.c") === 1
+jv.pointObjectValue = function (obj, path) {
+  if (!path) return obj;
+  jv._pointObject_ = obj;
+  var ret = eval("jv._pointObject_." + path);
+  delete jv._pointObject_;
+  return ret;
+}
+
+
+export default jv;
---
+++ src/vue-city.js
@@ -0,0 +1,197 @@
+import jv from './libjv'
+
+jv.citys = [{
+  c: 110100,
+  n: '北京市',
+  s: [{c: 110101, n: '东城区'}, {c: 110102, n: '西城区'}, {c: 110105, n: '朝阳区'}, {c: 110106, n: '丰台区'}, {
+    c: 110107,
+    n: '石景山区'
+  }, {c: 110108, n: '海淀区'}, {c: 110109, n: '门头沟区'}, {c: 110111, n: '房山区'}, {c: 110112, n: '通州区'}, {
+    c: 110113,
+    n: '顺义区'
+  }, {c: 110114, n: '昌平区'}, {c: 110115, n: '大兴区'}, {c: 110116, n: '怀柔区'}, {c: 110117, n: '平谷区'}, {
+    c: 110228,
+    n: '密云县'
+  }, {c: 110229, n: '延庆县'}]
+}, {
+  c: 120100,
+  n: '天津市',
+  s: [{c: 120101, n: '和平区'}, {c: 120102, n: '河东区'}, {c: 120103, n: '河西区'}, {c: 120104, n: '南开区'}, {
+    c: 120105,
+    n: '河北区'
+  }, {c: 120106, n: '红桥区'}, {c: 120110, n: '东丽区'}, {c: 120111, n: '西青区'}, {c: 120112, n: '津南区'}, {
+    c: 120113,
+    n: '北辰区'
+  }, {c: 120114, n: '武清区'}, {c: 120115, n: '宝坻区'}, {c: 120116, n: '滨海新区'}, {c: 120221, n: '宁河县'}, {
+    c: 120223,
+    n: '静海县'
+  }, {c: 120225, n: '蓟县'}]
+}, {c: 130000, n: '河北省'}, {c: 140000, n: '山西省'}, {c: 150000, n: '内蒙古自治区'}, {c: 210000, n: '辽宁省'}, {
+  c: 220000,
+  n: '吉林省'
+}, {c: 230000, n: '黑龙江省'}, {
+  c: 310100,
+  n: '上海市',
+  s: [{c: 310101, n: '黄浦区'}, {c: 310104, n: '徐汇区'}, {c: 310105, n: '长宁区'}, {c: 310106, n: '静安区'}, {
+    c: 310107,
+    n: '普陀区'
+  }, {c: 310108, n: '闸北区'}, {c: 310109, n: '虹口区'}, {c: 310110, n: '杨浦区'}, {c: 310112, n: '闵行区'}, {
+    c: 310113,
+    n: '宝山区'
+  }, {c: 310114, n: '嘉定区'}, {c: 310115, n: '浦东新区'}, {c: 310116, n: '金山区'}, {c: 310117, n: '松江区'}, {
+    c: 310118,
+    n: '青浦区'
+  }, {c: 310120, n: '奉贤区'}, {c: 310230, n: '崇明县'}]
+}, {c: 320000, n: '江苏省'}, {c: 330000, n: '浙江省'}, {c: 340000, n: '安徽省'}, {c: 350000, n: '福建省'}, {
+  c: 360000,
+  n: '江西省'
+}, {c: 370000, n: '山东省'}, {c: 410000, n: '河南省'}, {c: 420000, n: '湖北省'}, {c: 430000, n: '湖南省'}, {
+  c: 440000,
+  n: '广东省'
+}, {c: 450000, n: '广西壮族自治区'}, {c: 460000, n: '海南省'}, {c: 500000, n: '重庆'}, {c: 510000, n: '四川省'}, {
+  c: 520000,
+  n: '贵州省'
+}, {c: 530000, n: '云南省'}, {c: 540000, n: '西藏自治区'}, {c: 610000, n: '陕西省'}, {c: 620000, n: '甘肃省'}, {
+  c: 630000,
+  n: '青海省'
+}, {c: 640000, n: '宁夏回族自治区'}, {c: 650000, n: '新疆维吾尔自治区'}, {c: 710000, n: '台湾'}, {c: 810000, n: '香港特别行政区'}, {
+  c: 820000,
+  n: '澳门特别行政区'
+}]
+
+jv.citys.zhixia = [];
+
+jv.citys.forEach(it => {
+  if (it.c % 10000) {
+    jv.citys.zhixia.push(parseInt(it.c / 10000));
+  }
+});
+
+jv.citys.isZhixia = function (code) {
+  return jv.citys.zhixia.indexOf(parseInt(code / 10000)) >= 0;
+}
+
+jv.citys.recursion(it => it.s, it => {
+  if (it.c % 100) return;
+  if (it.s && it.s.length) return;
+  it.s = [];
+});
+
+jv.citys.getLevel = function (code) {
+  if (!code) return 0;
+  if (code % 100) return 3;
+  if (code % 10000) return 2;
+  return 1;
+}
+
+jv.citys.findByCode = function (code) {
+  if (!code) return;
+  code = parseInt(code);
+  //由于1级码可能是直辖市，所以.
+  // var level2Code = parseInt(code / 100) * 100;
+
+  var findSubOne = function (datas, code, level, maxLevel) {
+    if (level > 3) return null;
+    if (!datas) return null;
+    if (level > maxLevel) return null;
+
+    var chuShu = 1;
+    if (level == 1) chuShu = 10000;
+    else if (level == 2) chuShu = 100;
+
+    var levelCode = parseInt(code / chuShu) * chuShu;
+
+    for (var data of datas) {
+      if (data.c == code) {
+        return data;
+      }
+      if (data.c == levelCode) {
+        return findSubOne(data.s, code, level + 1, maxLevel);
+      }
+      // if (level == 1 && data.c == level2Code) {
+      //   return findSubOne(datas, code, level + 1);
+      // }
+    }
+    return null;
+  };
+
+  var level = jv.citys.getLevel(code);
+  var iszhi = jv.citys.isZhixia(code);
+  if (level == 1 && iszhi) {
+    return jv.citys.filter(it => parseInt(it.c / 10000) == parseInt(code / 10000))[0];
+  }
+  return findSubOne(jv.citys, code, iszhi ? 2 : 1, level);
+}
+
+jv.citys.loadChildCitys = function (code, ajax, loaded) {
+  code = parseInt(code);
+  if (code % 100) return;
+  var city = jv.citys.findByCode(code);
+  if (city && city.s && city.s.length) {
+    return;
+  }
+
+  ajax.post("/open/getChildCitys", {code: code})
+      .then(res => {
+        var json = res.data.data;
+
+        if (!( city.c % 10000)) {
+          json.forEach(it => {
+            it.s = [];
+          });
+        }
+
+        city.s = json;
+
+        if (loaded) {
+          loaded(city);
+        }
+      });
+};
+
+jv.citys.confirm = function (code, ajax, loaded) {
+  if (!code) return;
+  code = parseInt(code);
+  var city = jv.citys.findByCode(code);
+  if (city) return loaded(city);
+  var level = jv.citys.getLevel(code);
+  if (level >= 2) {
+    jv.citys.loadChildCitys(parseInt(code / 10000) * 10000, ajax, it => {
+      if (level == 3) {
+        jv.citys.loadChildCitys(parseInt(code / 100) * 100, ajax, loaded);
+      }
+    });
+  }
+}
+
+jv.citys.getEachCitys = function (code) {
+  var ret = [];
+  if (!code) return ret;
+  code = parseInt(code);
+
+  var level = jv.citys.getLevel(code);
+
+  var chuShu = 1;
+  for (var i = 1; i <= level; i++) {
+    if (i == 1) {
+      chuShu = 10000;
+    }
+    else if (i == 2) {
+      chuShu = 100;
+    }
+    else chuShu = 1;
+
+    var city = jv.citys.findByCode(parseInt(code / chuShu) * chuShu);
+    if (!city) {
+      if (i == 1) {
+        continue;
+      }
+      return ret;
+    }
+    ret.push({c: city.c, n: city.n});
+    if (city.c == code) break;
+  }
+  return ret;
+}
+
+export default jv;

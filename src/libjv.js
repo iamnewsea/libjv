aeeb564a1f14024a1640889430f263c9f72c0b41
于新海 <13466396863@163.com>
2017-11-08 18:01:04 +0800

    jv.await

--- src/libjv.js
+++ src/libjv.js
@@ -76,7 +76,7 @@ jv.await = function (delayTime, times, action) {
 
   if (action() !== false) {
     return setTimeout(() => {
-      jv.await(delayTime, action, times - 1);
+      jv.await(delayTime, times - 1, action);
     }, delayTime);
   }
 }

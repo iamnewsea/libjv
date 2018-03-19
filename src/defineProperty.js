cf4d18ffecfa058dde516ace0a364335c55c3cdf
于新海 <13466396863@163.com>
2018-03-19 11:58:20 +0800

    prototype 拆分.

--- src/defineProperty.js
+++ src/defineProperty.js
@@ -1,11 +1,10 @@
-import p_array from "prototype/Array"
-import p_date from "prototype/Date"
-import p_dom from "prototype/Dom"
-import p_number from "prototype/Number"
-import p_object from "prototype/Object"
-import p_set from "prototype/Set"
-import p_string from "prototype/String"
+import  "./prototype/Object"
+import  "./prototype/Number"
+import  "./prototype/String"
+import  "./prototype/Array"
+import  "./prototype/Date"
+import  "./prototype/Set"
+import  "./prototype/Dom"
 
 
-export default Object.assign({}, p_object,p_number,p_string,p_date,p_array,p_set,p_dom);
 

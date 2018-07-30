b226f6c69dfe3c9e357f81bfea375a1f5a64e581
于新海 <13466396863@163.com>
2018-07-30 10:49:54 +0800

    .

--- src/prototype/Object.js
+++ src/prototype/Object.js
@@ -1,31 +1,32 @@
 //-------------------------------------------------------------
 //返回 function ,string 等实际类名。
 Object.defineProperty(Object.prototype, "Type", {
-  get() {
-    var ret = this.constructor.name || typeof(this);
-    //把第一个非大写字母前面的全部变成小写字母。
+    get() {
+        var ret = this.constructor.name || typeof(this);
+        //把第一个非大写字母前面的全部变成小写字母。
+        //不宜使用 this.prototype.constructor.name
 
-    if (ret) {
-      var index = 0;
-      for (var it of ret) {
-        var code = it.charCodeAt();
-        if (code >= 65 && code <= 90) {
-          index++;
-        }
-        else {
-          break;
-        }
-      }
+        if (ret) {
+            var index = 0;
+            for (var it of ret) {
+                var code = it.charCodeAt();
+                if (code >= 65 && code <= 90) {
+                    index++;
+                }
+                else {
+                    break;
+                }
+            }
 
-      if (index) {
-        if (index > 1) {
-          index--;
+            if (index) {
+                if (index > 1) {
+                    index--;
+                }
+                ret = ret.slice(0, index).toLocaleLowerCase() + ret.slice(index);
+            }
         }
-        ret = ret.slice(0, index).toLocaleLowerCase() + ret.slice(index);
-      }
-    }
-    return ret;
-  }, enumerable: false
+        return ret;
+    }, enumerable: false
 });
 
 
@@ -34,36 +35,36 @@ Object.defineProperty(Object.prototype, "Type", {
  data.sex_res == "男"
  */
 Object.defineProperty(Object.prototype, "Enumer", {
-  value(key, enumDef, override) {
-    var obj = this;
-    if (key in obj == false) {
-      throw new Error("找不到 " + key + " 属性(" + enumDef.type + ")")
-    }
-    var p = obj[key];
-    var v = enumDef.getData(p.toString());
-    if (!v) {
-      return;
-    }
-    if (override) {
-      obj[key] = v.remark || "";
-    }
-    else {
-      obj[key + "_res"] = v.remark || "";
-    }
-  }, enumerable: false
+    value(key, enumDef, override) {
+        var obj = this;
+        if (key in obj == false) {
+            throw new Error("找不到 " + key + " 属性(" + enumDef.type + ")")
+        }
+        var p = obj[key];
+        var v = enumDef.getData(p.toString());
+        if (!v) {
+            return;
+        }
+        if (override) {
+            obj[key] = v.remark || "";
+        }
+        else {
+            obj[key + "_res"] = v.remark || "";
+        }
+    }, enumerable: false
 });
 
 
 //大于等于 and 小于等于
 Object.defineProperty(Object.prototype, "Between", {
-  value(start, end) {
-    if (start > end) {
-      var t = start;
-      start = end;
-      end = t;
-    }
+    value(start, end) {
+        if (start > end) {
+            var t = start;
+            start = end;
+            end = t;
+        }
 
-    if (this < start) return false;
-    if (this > end) return false;
-  }, enumerable: false
+        if (this < start) return false;
+        if (this > end) return false;
+    }, enumerable: false
 });

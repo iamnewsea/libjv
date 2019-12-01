ff2788a54c638a10d703a1940c5ef587f1088a28
于新海 <yuxinhai@lwljuyang.com>
2019-12-01 11:36:52 +0800

    .

--- libjv/main.js
+++ libjv/main.js
@@ -1,7 +1,7 @@
 //module.exports = require('./lib/libjv');sss
 
 import vue_chk from "./lib/vue-chk"
-import vue_city from "./lib/vue-city"
+// import vue_city from "./lib/vue-city"
 import file_upload from "./lib/file-upload"
 
 //必须这样写, 如果写成 Object.assign({}, vue_chk, vue_city, file_upload); enum.js 扩展不了 jv.

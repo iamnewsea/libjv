9d49b80db894c168cdb6373ca9f1c13ce9ff3203
于新海 <yuxinhai@lwljuyang.com>
2019-12-08 17:44:50 +0800

    .

--- element-ui-ext/packages/selector.vue
+++ element-ui-ext/packages/selector.vue
@@ -2,15 +2,15 @@
   <div class="enum" @dblclick="value2 = ''">
     <template v-if="mode == 'radio'">
       <el-radio-group v-model="value2" v-bind="[$props, $attrs]">
-        <el-radio v-for="(item,index) in data" :label="keyField ? item[keyField] : item "
-                  :key="index">{{valueField ? item[valueField] : item}}
+        <el-radio v-for="(value,key) in data2" :label="key"
+                  :key="key">{{value}}
         </el-radio>
       </el-radio-group>
     </template>
     <template v-else>
       <el-checkbox-group v-model="value2" v-bind="[$props, $attrs]">
-        <el-checkbox v-for="(item,index) in data" :label="keyField ? item[keyField] : item "
-                     :key="index">{{valueField ? item[valueField] : item}}
+        <el-checkbox v-for="(value,key) in data2" :label="key"
+                     :key="key">{{value}}
         </el-checkbox>
       </el-checkbox-group>
     </template>
@@ -20,10 +20,11 @@
     export default {
         name: "selector",
         props: {
-            data: {type: Array, default: []},
+            data: {type: [Object, Array], default: []},
+            //当 data 是 Array的时候，需要指定 field中的两个值 。 当data是Object的时候，指定 一个field 表示显示的值
             field: {
                 type: String, default() {
-                    return "id,name"
+                    return ""
                 }
             },
             mode: {type: String, default: "radio"},
@@ -54,15 +55,43 @@
         },
         data() {
             return {
+                data2: {},
                 value2: this.mode == "radio" ? "" : [],
                 keyField: "",
                 valueField: ""
             };
         },
-        mounted() {
+        created() {
             var fields = (this.field || "").split(",")
             this.keyField = fields[0];
             this.valueField = fields[1];
+
+            this.setData();
+
+        }, methods: {
+            setData() {
+                if (jv.IsNull(this.data)) {
+                    this.data2 = {};
+                    return;
+                }
+
+                var dataType = this.data.Type;
+
+                var d2 = {};
+                if (["set", "array"].includes(dataType)) {
+                    for (var i = 0, len = this.data.length; i < len; i++) {
+                        var item = this.data[i];
+                        d2[this.keyField ? item[this.keyField] : item] = this.valueField ? item[this.valueField] : item;
+                    }
+                } else {
+                    for (var key of Object.keys(this.data)) {
+                        var value = this.data[key];
+                        d2[key] = this.keyField ? value[this.keyField] : value;
+                    }
+                }
+
+                this.data2 = d2;
+            }
         }
     }
 </script>

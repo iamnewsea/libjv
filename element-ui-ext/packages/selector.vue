2b9b51f0b37aa62b2c174376b28c349e3fc49fd7
于新海 <13466396863@163.com>
2019-12-22 18:41:45 +0800

    .

--- element-ui-ext/packages/selector.vue
+++ element-ui-ext/packages/selector.vue
@@ -17,83 +17,105 @@
   </div>
 </template>
 <script>
-    export default {
-        name: "selector",
-        props: {
-            data: {type: [Object, Array], default: []},
-            //当 data 是 Array的时候，需要指定 field中的两个值 。 当data是Object的时候，指定 一个field 表示显示的值
-            field: {
-                type: String, default() {
-                    return ""
-                }
-            },
-            mode: {type: String, default: "radio"},
-            value: {
-                type: [String, Array, Object], default() {
-                    return ""
-                }
-            }
-        },
-        watch: {
-            value: {
-                deep: true, handler(v) {
-                    if (jv.dataEquals(v, this.value2)) {
-                        return;
-                    }
-                    this.value2 = v;
-                }
-            },
-            value2: {
-                deep: true, handler(v) {
-
-                    if (jv.dataEquals(v, this.value)) {
-                        return;
-                    }
-                    this.$emit("input", v);
-                }
-            }
-        },
-        data() {
-            return {
-                data2: {},
-                value2: this.mode == "radio" ? "" : [],
-                keyField: "",
-                valueField: ""
-            };
-        },
-        created() {
-            var fields = (this.field || "").split(",")
-            this.keyField = fields[0];
-            this.valueField = fields[1];
-            this.value2 = this.value;
-            this.setData();
-
-        }, methods: {
-            setData() {
-                if (jv.IsNull(this.data)) {
-                    this.data2 = {};
-                    return;
-                }
-
-                var dataType = this.data.Type;
-
-                var d2 = {};
-                if (["set", "array"].includes(dataType)) {
-                    for (var i = 0, len = this.data.length; i < len; i++) {
-                        var item = this.data[i];
-                        d2[this.keyField ? item[this.keyField] : item] = this.valueField ? item[this.valueField] : item;
-                    }
-                } else {
-                    for (var key of Object.keys(this.data)) {
-                        var value = this.data[key];
-                        d2[key] = this.keyField ? value[this.keyField] : value;
-                    }
-                }
-
-                this.data2 = d2;
-            }
+  export default {
+    name: "selector",
+    props: {
+      url: {type: String, default: ""},
+      data: {type: [Object, Array], default: []},
+      //当 data 是 Array的时候，需要指定 field中的两个值 。 当data是Object的时候，指定 一个field 表示显示的值
+      field: {
+        type: String, default() {
+          return ""
         }
+      },
+      mode: {type: String, default: "radio"},
+      value: {
+        type: [String, Array, Object], default() {
+          return ""
+        }
+      }
+    },
+    watch: {
+      data: {
+        deep: true, handler(v) {
+          if (jv.dataEquals(v, this.data2)) {
+            return;
+          }
+          this.setData(v);
+        }
+      },
+      url(v) {
+        if (jv.dataEquals(v, this.url)) {
+          return;
+        }
+        this.$http.post(v).then(res => {
+          this.setData(res.data.data);
+        });
+      },
+      value: {
+        deep: true, handler(v) {
+          if (jv.dataEquals(v, this.value2)) {
+            return;
+          }
+          this.value2 = v;
+        }
+      },
+      value2: {
+        deep: true, handler(v) {
+
+          if (jv.dataEquals(v, this.value)) {
+            return;
+          }
+          this.$emit("input", v);
+        }
+      }
+    },
+    data() {
+      return {
+        data2: {},
+        value2: this.mode == "radio" ? "" : [],
+        keyField: "",
+        valueField: ""
+      };
+    },
+    created() {
+      var fields = (this.field || "").split(",")
+      this.keyField = fields[0];
+      this.valueField = fields[1];
+      this.value2 = this.value;
+      this.init();
+
+    }, methods: {
+      init() {
+        if (this.url) {
+          this.$http.post(this.url).then(res => {
+            this.setData(res.data.data);
+          });
+        } else {
+          this.setData(this.data);
+        }
+      },
+      setData(data) {
+        if (jv.IsNull(data)) return;
+        var dataType = data.Type;
+
+        var d2 = {};
+        if (["set", "array"].includes(dataType)) {
+          for (var i = 0, len = data.length; i < len; i++) {
+            var item = data[i];
+            d2[this.keyField ? item[this.keyField] : item] = this.valueField ? item[this.valueField] : item;
+          }
+        } else {
+          for (var key of Object.keys(data)) {
+            var value = data[key];
+            d2[key] = this.keyField ? value[this.keyField] : value;
+          }
+        }
+
+        this.data2 = d2;
+      }
     }
+  }
 </script>
 <style>
   .v .enum {

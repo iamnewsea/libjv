e5fb7153d1ed02b53ec673a574fe94a97b27442f
于新海 <yuxinhai@lwljuyang.com>
2020-03-16 16:34:27 +0800

    .

--- element-ui-ext/src/right-menu.js
+++ element-ui-ext/src/right-menu.js
@@ -1,3 +1,10 @@
+/**
+ * 扩展Vue组件的右键属性，使支持右键。
+ * 使用方式：
+ * 1. 组件上定义：v-menu:div1。
+ * 2. 定义 ref=div1 的组件。
+ */
+
 export default {
   install(Vue) {
     Vue.directive('menu', {
@@ -5,23 +12,21 @@ export default {
         el.addEventListener("contextmenu", function (ev) {
           ev.preventDefault();
           var menu = vnode.context.$refs[binding.arg];
-          menu.style.display = "block";
           menu.style.left = ev.pageX + "px";
           menu.style.top = ev.pageY + "px";
-
+          menu.classList.add("menu-shop");
 
 
           setTimeout(() => {
-            var chkEvent = jv.createEvent("show",{});
+            var chkEvent = jv.createEvent("show", {});
             menu.trigger(chkEvent);
 
             document.once("click", function (event) {
               var menu = vnode.context.$refs[binding.arg];
               if (menu) {
-                menu.style.display = "none";
-
-                var chkEvent = jv.createEvent("hide",{});
+                var chkEvent = jv.createEvent("hide", {});
                 menu.trigger(chkEvent);
+                menu.classList.add("menu-hide");
               }
             });
           }, 0);

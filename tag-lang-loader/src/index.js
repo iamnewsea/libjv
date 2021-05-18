1bba1fbb8965c4afbb4101016e8825756feb48aa
于新海 <iamnewsea@163.com>
2021-05-18 21:27:23 +0800

    tag-lang-loader

--- tag-lang-loader/src/index.js
+++ tag-lang-loader/src/index.js
@@ -1,13 +1,14 @@
 module.exports = function (source) {
-    var langs = (process.env.VUE_APP_All_Langs || "").split(",").filter(it => it).map(it=>it.trim())
+    var langs = (process.env.VUE_APP_All_Langs || "").split(",").filter(it => it).map(it => it.trim())
     if (!langs.length) return source;
 
     var lang = process.env.VUE_APP_Lang || "";
+
     if (!lang) return source;
 
     var filePath = this.resourcePath;
     if (langs.includes(lang) == false) {
-        throw new Error( filePath + " 不存在的语言代码 " + lang)
+        throw new Error(filePath + " 不存在的语言代码 " + lang)
     }
 
     var removeLangs = langs.filter(it => it != lang);

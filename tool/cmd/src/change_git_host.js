0fdfa0db4d10cf03721f84abeb0012e4707c44ce
于新海 <iamnewsea@163.com>
2021-06-02 00:56:31 +0800

    .

--- tool/cmd/src/change_git_host.js
+++ tool/cmd/src/change_git_host.js
@@ -28,31 +28,37 @@ if (!cmd.source || !cmd.target) {
 }
 
 if (!cmd.deep) {
-    cmd.deep = 2;
+    cmd.deep = 5;
 }
 
 var path_abs = path.resolve(cmd.path);
 
-debugger;
+
 util.walkFile(path_abs, async (filePath, isFile) => {
-    if (isFile) return false;
-    if (filePath.endsWith("node_modules")) return false;
-    if (filePath.endsWith(".git")) return false;
+    if (isFile) return null;
+    if (filePath.endsWith("node_modules")) return null;
+    if (filePath.endsWith(".git")) return null;
 
     var deep = filePath.slice(path_abs.length).trimWith(path.sep).split(path.sep).length;
 
-    if( deep > cmd.deep) return false;
+    if (deep > cmd.deep) return null;
 
     if (fs.existsSync(filePath + path.sep + ".git")) {
         process.chdir(filePath);
         var gitPath = await util.execCmd(get_url_cmd);
-        if( !gitPath.includes(cmd.source)){
+        gitPath = gitPath.replaceAll("\n", "")
+        if (!gitPath.includes(cmd.source)) {
+            if (gitPath.includes(cmd.target)) {
+                print(gitPath + " 已经设置了")
+            } else {
+                print("跳过 " + gitPath)
+            }
             return;
         }
 
         var newGitPath = gitPath.replaceAll(cmd.source, cmd.target);
 
-        print(gitPath + " : " + gitPath + " --> " + newGitPath);
+        print(gitPath + " --> " + newGitPath);
         // if (await util.readLine() != "y") return;
         await util.execCmd(set_url_cmd.format({url: newGitPath}));
     }

0b6afdd7b61172fe0688e54d05aad15f21317f1e
于新海 <iamnewsea@163.com>
2021-06-07 10:35:38 +0800

    webpart

---
+++ webpart/packages/c.vue
@@ -0,0 +1,13 @@
+<template>
+  <span v-bind="$attrs" v-on="$listeners">
+    <slot></slot>
+  </span>
+</template>
+<script>
+  export default {
+    name: "c",
+    data() {
+      return {};
+    }
+  }
+</script>

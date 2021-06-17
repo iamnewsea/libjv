2739747755fd0e5cf506dc41022692de2b4e6e84
于新海 <iamnewsea@163.com>
2021-06-17 17:20:43 +0800

    .

--- libjv-loader/src/vars.js
+++ libjv-loader/src/vars.js
@@ -1,20 +1,20 @@
 module.exports = function (source) {
 
     var vars = {};
-    vars.push("YEAR", (new Date()).getFullYear())
-    vars.push("BUILDAT", Date.now().toDateString())
+    vars["YEAR"] = (new Date()).getFullYear()
+    vars["BUILDAT"] = Date.now().toDateString()
 
     var modeIndex = process.argv.indexOf("--mode");
     if (modeIndex > 0 && modeIndex < process.argv.length - 1) {
         var mode = process.argv[modeIndex + 1];
 
-        vars.push("ENVNAME", mode)
+        vars["ENVNAME"] = mode
     }
 
     var envs = process.env
     Object.keys(envs).filter(it => it.startsWith("VUE_APP_")).forEach(it => {
         var key2 = key.slice(8);
-        vars.push(key2, envs[key2]);
+        vars[key2] = envs[key2];
     })
 
     source = source.replace(/@([\w_]*)@/ig, (match, groupValue, startIndex) => {

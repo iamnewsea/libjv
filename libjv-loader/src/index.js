24242301b3a6f91b3db1744f7bddc9925392c2c8
于新海 <iamnewsea@163.com>
2021-06-17 17:18:45 +0800

    .

--- libjv-loader/src/index.js
+++ libjv-loader/src/index.js
@@ -1,23 +1,12 @@
 var inline = require("./inline-res")
 var tagLag = require("./tag-lang")
+var vars = require("./vars")
 var jv = require("libjv")
 
 
 module.exports = function (source) {
-    //变量
-    source = source.replaceAll("@YEAR@", (new Date()).getFullYear())
-    source = source.replaceAll("@BUILDAT@", Date.now().toDateString())
-
-    var modeIndex = process.argv.indexOf("--mode");
-    if (modeIndex > 0 && modeIndex < process.argv.length - 1) {
-        var mode = process.argv[modeIndex + 1];
-
-        source = source.replaceAll("@ENVNAME@", mode)
-    }
-
-
+    source = vars.call(this,source);
     source = inline.call(this, source);
     source = tagLag.call(this, source);
-
     return source;
 }
---
+++ libjv-loader/src/vars.js
@@ -0,0 +1,27 @@
+module.exports = function (source) {
+
+    var vars = {};
+    vars.push("YEAR", (new Date()).getFullYear())
+    vars.push("BUILDAT", Date.now().toDateString())
+
+    var modeIndex = process.argv.indexOf("--mode");
+    if (modeIndex > 0 && modeIndex < process.argv.length - 1) {
+        var mode = process.argv[modeIndex + 1];
+
+        vars.push("ENVNAME", mode)
+    }
+
+    var envs = process.env
+    Object.keys(envs).filter(it => it.startsWith("VUE_APP_")).forEach(it => {
+        var key2 = key.slice(8);
+        vars.push(key2, envs[key2]);
+    })
+
+    source = source.replace(/@([\w_]*)@/ig, (match, groupValue, startIndex) => {
+        if (groupValue in vars) return vars[groupValue];
+        if (groupValue in envs) return envs[groupValue]
+        return it;
+    })
+
+    return source;
+}

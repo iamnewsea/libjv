cb830bbc950cad163812e619b6580362166f1e26
于新海 <iamnewsea@163.com>
2021-06-17 16:56:44 +0800

    .

--- libjv-loader/src/index.js
+++ libjv-loader/src/index.js
@@ -1,9 +1,23 @@
 var inline = require("./inline-res")
 var tagLag = require("./tag-lang")
+var jv = require("libjv")
+
 
 module.exports = function (source) {
+    //变量
+    source = source.replaceAll("@YEAR@", (new Date()).getFullYear())
+    source = source.replaceAll("@BUILDAT@", Date.now().toDateString())
+
+    var modeIndex = process.argv.indexOf("--mode");
+    if (modeIndex > 0 && modeIndex < process.argv.length - 1) {
+        var mode = process.argv[modeIndex + 1];
+
+        source = source.replaceAll("@ENVNAME@", mode)
+    }
+
+
     source = inline.call(this, source);
-    source = tagLag.call(this,source);
+    source = tagLag.call(this, source);
 
     return source;
 }

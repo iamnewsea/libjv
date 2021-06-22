1a83fdfd9576ed8b2f6a224be1d5865fb3f84c33
于新海 <iamnewsea@163.com>
2021-06-22 16:00:52 +0800

    .

--- libjv-loader/src/index.js
+++ libjv-loader/src/index.js
@@ -5,7 +5,12 @@ var jv = require("libjv")
 
 
 module.exports = function (source) {
-    source = vars.call(this,source);
+    if (this.resourceQuery) {
+        return source;
+    }
+
+    // source = hi.call(this,source);
+    source = vars.call(this, source);
     source = inline.call(this, source);
     source = tagLag.call(this, source);
     return source;

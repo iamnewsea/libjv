c138c331ed82c79a016e4c40bc6e67227a5958d5
于新海 <iamnewsea@163.com>
2021-06-23 11:13:48 +0800

    .

--- libjv-plugin/src/hi.js
+++ libjv-plugin/src/hi.js
@@ -1,25 +1,32 @@
-var path = require("path");
-var fs = require("./async_fs");
-var jv = require("libjv")
+module.exports = function (compilation, callback) {
+    var path = require("path");
+    var fs = require("fs");
+    var jv = require("libjv")
 
-module.exports = function (compilation, options) {
-    var package_json_path = compilation.options.context + path.sep + "package.json";
 
-    return fs.existsPromise(package_json_path).then(v => {
-        if (v == false) {
-            return;
+    var vars = {};
+    vars["BUILDAT"] = Date.now().toDateString()
+
+    var modeIndex = process.argv.indexOf("--mode");
+    if (modeIndex > 0 && modeIndex < process.argv.length - 1) {
+        var mode = process.argv[modeIndex + 1];
+
+        vars["ENVNAME"] = mode
+    }
+
+    var hi_content = fs.readFileSync(path.join(__dirname, "../hi.html"), 'utf-8')
+        .replaceAll("@PROJECTNAME@", process.env.npm_package_name)
+        .replaceAll("@ENVNAME@", vars["ENVNAME"])
+        .replaceAll("@BUILDAT@", vars["BUILDAT"])
+
+    // 将这个列表作为一个新的文件资源，插入到 webpack 构建中：
+    compilation.assets['hi.html'] = {
+        source: function () {
+            return hi_content;
+        },
+        size: function () {
+            return hi_content.length;
         }
-
-        var json = JSON.parse(fs.readFileSync(package_json_path, "utf-8"));
-
-        var hi_content = fs.readFileSync(path.join(__dirname, "../hi.html"), 'utf-8')
-            .replaceAll("@projectName@", json.name)
-            .replaceAll("@projectVersion@", json.version)
-            .replaceAll("@projectDescription@", json.description)
-            .replaceAll("@buildAt@", Date.now().toDateString())
-
-        fs.writeFileSync(path.join(compilation.options.context, "public/hi.html"), hi_content);
-
-        return;
-    })
+    };
+    callback();
 }
--- libjv-plugin/src/index.js
+++ libjv-plugin/src/index.js
@@ -1,18 +1,10 @@
 var hi = require("./hi")
 
 class LibjvPlugin {
-    options;
-
-    constructor(options) {
-        this.options = options;
-    }
-
-    apply(compiler) {
-        // 在emit阶段插入钩子函数
-        compiler.hooks.run.tapPromise('LibjvPlugin', (compilation) =>  {
-            return hi(compilation,this.options);
-        });
+    apply(compiler){
+        compiler.hooks.emit.tapAsync('LibjvPlugin', (compilation, callback)=>{
+            hi(compilation,callback);
+        })
     }
 }
-
 module.exports = LibjvPlugin

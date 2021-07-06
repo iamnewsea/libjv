4409ff979e302512b93fdb3709d86e4afaba79d4
于新海 <iamnewsea@163.com>
2021-07-06 21:04:37 +0800

    .

--- libjv-plugin/src/index.js
+++ libjv-plugin/src/index.js
@@ -8,15 +8,24 @@ class LibjvPlugin {
 
     apply(compiler) {
         compiler.hooks.emit.tapAsync('LibjvPlugin', (compilation, callback) => {
+            var list = [];
             if (this.options.hi !== false) {
-                return hi(compilation, callback);
+                list.push(hi);
             }
 
             if (this.options.dlgIframe !== false) {
-                return dlg_iframe(compilation, callback);
+                list.push(dlg_iframe);
             }
 
-            callback();
+            var set_count = 0;
+            list.forEach(it => {
+                it(compilation, it => {
+                    set_count++;
+                    if (list.length == set_count) {
+                        callback();
+                    }
+                });
+            })
         })
     }
 }
